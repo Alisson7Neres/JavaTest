727a52c8b2ee957e9fe5913fc30a92cc8125c81c
Alisson7Neres <alisson.neres@hotmail.com>
2022-02-04 11:19:39 -0300

    Conectando com SGBD, fazendo mapeamento no application

--- javaTest/src/main/java/com/cd2/JavaTestApplication.java
+++ javaTest/src/main/java/com/cd2/JavaTestApplication.java
@@ -1,9 +1,19 @@
 package com.cd2;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"com.cd2.domain"})
+@ComponentScan(basePackages = {"com.cd2.service", "com.cd2.exceptions"})
+@EnableJpaRepositories(basePackages = {"com.cd2.repository"})
+@EnableAutoConfiguration
+@EnableConfigurationProperties
 public class JavaTestApplication {
 
 	public static void main(String[] args) {
