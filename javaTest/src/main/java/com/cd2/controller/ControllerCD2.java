ed79e103fae0faf8c586a89ed92893ca31559c45
Alisson7Neres <alisson.neres@hotmail.com>
2022-02-06 19:34:09 -0300

    Implementado as regras de negócios

--- javaTest/src/main/java/com/cd2/controller/ControllerCD2.java
+++ javaTest/src/main/java/com/cd2/controller/ControllerCD2.java
@@ -1,6 +1,15 @@
 package com.cd2.controller;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import javax.validation.Valid;
@@ -19,6 +28,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.cd2.domain.Model;
 import com.cd2.service.Servico;
+import com.google.gson.Gson;
 
 @Controller
 @RequestMapping(value = "/sigabem")
@@ -39,10 +49,59 @@ public class ControllerCD2 {
 		Model model = servico.findById(id);
 		return ResponseEntity.ok().body(model);
 	}
-
+	
 	@PostMapping(value = "/")
-	public ResponseEntity<Model> create(@Valid @RequestBody Model model) {
+	public ResponseEntity<Model> create(@Valid @RequestBody Model model) throws ParseException {
+
+		if(model.getCepDestino() != null) {
+			cepDestino(model);
+		} if (model.getCepOrigem() != null) {
+			cepOrigem(model);
+		}
+		String dddOrigem = model.getDddOrigem();
+		String dddDestino = model.getDddDestino();
+		String ufOrigem = model.getUfOrigem(); 
+		String ufDestino = model.getUfDestino();
+		
+
+		
+		if(dddOrigem.equals(dddDestino) && ufOrigem.equals(ufDestino)) {
+			Double desconto = model.getVlTotalFrete();
+			desconto *= 1.0;
+			desconto *= 0.75;
+			model.getDataPrevistaEntrega();
+			model.setVlTotalFrete(desconto);
+			
+			dataPrevistaEstado(model);
+			
+			
+			if(dddOrigem.equals(dddDestino)) {
+				Double desconto2 = model.getVlTotalFrete();
+				desconto2 *= 0.5;
+				model.setVlTotalFrete(desconto2);
+				
+				dataPrevistaDdd(model);
+			}
+		}
+		
+		if(!ufOrigem.equals(ufDestino)) {
+			Double desconto = model.getVlTotalFrete();
+			desconto *= 1.0;
+			
+			model.setVlTotalFrete(desconto);
+			
+			dataPrevista(model);
+		}
+		
+		Date d = new Date();
+		DateFormat.getInstance().format(new Date());
+
+		model.getDataConsulta();
+		model.setDataConsulta(d);
+		
+		pesoTaxa(model);
 		model = servico.create(model);
+		
 		URI uri = ServletUriComponentsBuilder.fromCurrentContextPath().path("/{id}").buildAndExpand(model.getId()).toUri();
 		return ResponseEntity.created(uri).body(model);
 	}
@@ -52,4 +111,126 @@ public class ControllerCD2 {
 		servico.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	public void cepDestino(Model model) {
+		
+		try {
+			URL url = new URL("https://viacep.com.br/ws/"+model.getCepDestino()+"/json/");
+			URLConnection connection = url.openConnection();
+			InputStream is = connection.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+			
+			StringBuilder jsonCEP = new StringBuilder();
+			
+			String cepDestino = "";
+			
+			while((cepDestino = br.readLine()) != null) {
+				jsonCEP.append(cepDestino);
+			}
+			
+			System.out.println(jsonCEP.toString());
+			
+			Model aux = new Gson().fromJson(jsonCEP.toString(), Model.class);
+	
+			model.setCep(aux.getCep());
+			model.setDdd(aux.getDdd());
+			model.setUf(aux.getUf());
+			model.setDddDestino(aux.getDdd());
+			model.setUfDestino(aux.getUf());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void cepOrigem(Model model) {
+		
+		try {
+			URL url = new URL("https://viacep.com.br/ws/"+model.getCepOrigem()+"/json/");
+			URLConnection connection = url.openConnection();
+			InputStream is = connection.getInputStream();
+			BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+			
+			StringBuilder jsonCEP = new StringBuilder();
+			
+			String cepOrigem = "";
+			
+			while((cepOrigem = br.readLine()) != null) {
+				jsonCEP.append(cepOrigem);
+			}
+			
+			System.out.println(jsonCEP.toString());
+			
+			Model aux = new Gson().fromJson(jsonCEP.toString(), Model.class);
+	
+			model.setCep(aux.getCep());
+			model.setDdd(aux.getDdd());
+			model.setUf(aux.getUf());
+			model.setDddOrigem(aux.getDdd());
+			model.setUfOrigem(aux.getUf());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void pesoTaxa(@Valid @RequestBody Model model) {
+		Double peso = model.getPeso();
+		peso *= 1.0;
+		model.setVlTotalFrete(peso);
+		Double frete  = model.getVlTotalFrete();
+		Math.floor(frete);
+	}
+	
+	public void dataPrevistaDdd(@Valid @RequestBody Model model) {
+		
+		Date d = new Date();
+		DateFormat.getInstance().format(new Date());
+		
+		model.getDataConsulta();
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(d);
+		cal.add(Calendar.DATE, 1);
+		d = cal.getTime();
+		
+		model.setDataConsulta(d);
+		
+		model.setDataPrevistaEntrega(model.getDataConsulta());
+	}
+	
+	public void dataPrevistaEstado(@Valid @RequestBody Model model) {
+		
+		Date d = new Date();
+		DateFormat.getInstance().format(new Date());
+
+		model.getDataConsulta();
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(d);
+		cal.add(Calendar.DATE, 3);
+		d = cal.getTime();
+		
+		model.setDataConsulta(d);
+		
+		model.setDataPrevistaEntrega(model.getDataConsulta());
+	}
+	
+	public void dataPrevista(@Valid @RequestBody Model model) {
+		
+		Date d = new Date();
+		DateFormat.getInstance().format(new Date());
+		
+		model.getDataConsulta();
+		
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(d);
+		cal.add(Calendar.DATE, 10);
+		d = cal.getTime();
+		
+		model.setDataConsulta(d);
+		
+		model.setDataPrevistaEntrega(model.getDataConsulta());
+	}
 }
+
--- javaTest/src/main/java/com/cd2/domain/Model.java
+++ javaTest/src/main/java/com/cd2/domain/Model.java
@@ -1,11 +1,14 @@
 package com.cd2.domain;
 
 import java.io.Serializable;
+import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class Model implements Serializable{
@@ -16,27 +19,51 @@ public class Model implements Serializable{
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	private double  peso;
+	
 	private String cepOrigem;
 	private String cepDestino;
+	
+	private String cep;
+	private String dddOrigem;
+	private String dddDestino;
+	private String ufDestino;
+	private String ufOrigem;
+	
+	private String ddd;
+	private String uf;
+	
 	private String nomeDestinatario;
+	
 	private double vlTotalFrete;
-	private String dataPrevistaEntrega;
-	private String dataConsulta;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private Date dataPrevistaEntrega;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
+	private Date dataConsulta;
 	
 	public Model() {
 		super();
 	}
-	public Model(double peso, String cepOrigem, String cepDestino, String nomeDestinatario, 
-			double vlTotalFrete, String dataPrevistaEntrega, String dataConsulta, Integer id) {
+	public Model(double peso, String cepOrigem, String cepDestino, String nomeDestinatario, String cep,
+			 String dddOrigem, String dddDestino, String ufDestino, String ufOrigem, String ddd, String uf,
+			double vlTotalFrete, Date dataPrevistaEntrega, Date dataConsulta, Integer id) {
 		super();
+		this.id = id;
 		this.peso = peso;
 		this.cepOrigem = cepOrigem;
 		this.cepDestino = cepDestino;
+		this.cep = cep;
+		this.ddd = ddd;
+		this.dddOrigem = dddOrigem;
+		this.dddDestino = dddDestino;
+		this.ufDestino = ufDestino;
+		this.ufOrigem = ufOrigem;
+		this.uf = uf;
 		this.nomeDestinatario = nomeDestinatario;
 		this.vlTotalFrete = vlTotalFrete;
 		this.dataPrevistaEntrega = dataPrevistaEntrega;
 		this.dataConsulta = dataConsulta;
-		this.id = id;
 		
 	}
 	public double getPeso() {
@@ -57,6 +84,48 @@ public class Model implements Serializable{
 	public void setCepDestino(String cepDestino) {
 		this.cepDestino = cepDestino;
 	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+	public String getDdd() {
+		return ddd;
+	}
+	public void setDdd(String ddd) {
+		this.ddd = ddd;
+	}
+	public String getDddOrigem() {
+		return dddOrigem;
+	}
+	public void setDddOrigem(String dddOrigem) {
+		this.dddOrigem = dddOrigem;
+	}
+	public String getDddDestino() {
+		return dddDestino;
+	}
+	public void setDddDestino(String dddDestino) {
+		this.dddDestino = dddDestino;
+	}
+	public String getUfDestino() {
+		return ufDestino;
+	}
+	public void setUfDestino(String ufDestino) {
+		this.ufDestino = ufDestino;
+	}
+	public String getUfOrigem() {
+		return ufOrigem;
+	}
+	public void setUfOrigem(String ufOrigem) {
+		this.ufOrigem = ufOrigem;
+	}
+	public String getUf() {
+		return uf;
+	}
+	public void setUf(String uf) {
+		this.uf = uf;
+	}
 	public String getNomeDestinatario() {
 		return nomeDestinatario;
 	}
@@ -69,16 +138,16 @@ public class Model implements Serializable{
 	public void setVlTotalFrete(double vlTotalFrete) {
 		this.vlTotalFrete = vlTotalFrete;
 	}
-	public String getDataPrevistaEntrega() {
+	public Date getDataPrevistaEntrega() {
 		return dataPrevistaEntrega;
 	}
-	public void setDataPrevistaEntrega(String dataPrevistaEntrega) {
+	public void setDataPrevistaEntrega(Date dataPrevistaEntrega) {
 		this.dataPrevistaEntrega = dataPrevistaEntrega;
 	}
-	public String getDataConsulta() {
+	public Date getDataConsulta() {
 		return dataConsulta;
 	}
-	public void setDataConsulta(String dataConsulta) {
+	public void setDataConsulta(Date dataConsulta) {
 		this.dataConsulta = dataConsulta;
 	}
 	public Integer getId() {
@@ -86,6 +155,15 @@ public class Model implements Serializable{
 	}
 	public void setId(Integer id) {
 		this.id = id;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return "Model [peso=" + peso + ", cepOrigem=" + cepOrigem + ", cepDestino=" + cepDestino + ", dddOrigem="
+				+ dddOrigem + ", dddDestino=" + dddDestino + ", ufDestino=" + ufDestino + ", ufOrigem=" + ufOrigem
+				+ ", nomeDestinatario=" + nomeDestinatario + ", vlTotalFrete=" + vlTotalFrete + ", dataPrevistaEntrega="
+				+ dataPrevistaEntrega + ", dataConsulta=" + dataConsulta + "]";
 	}
 	
 	@Override
@@ -111,5 +189,4 @@ public class Model implements Serializable{
 			return false;
 		return true;
 	}
-	
 }
